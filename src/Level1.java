af0ec1e161f2d53c0fdc8c841249925c3684966f
MajesticNthg <majestic_nthg@mail.ru>
2024-05-13 22:29:05 +1000

    Initial commit

---
+++ src/Level1.java
@@ -0,0 +1,16 @@
+public class Level1 {
+    public static void MatrixTurn (String Matrix[], int M, int N, int T) {
+        
+    }
+    public static void main(String[] args) {
+        String[] Matrix = new String[] {"123456", "234567", "345678", "456789"};
+        char[] s = Matrix[1].toCharArray();
+        System.out.println(s);
+        int M = 4;
+        int N = 6;
+        int T = 3;
+
+        int f = 0;
+
+    }
+}
